051efe8e9b5675846b557919e350b4f1fd2dd718
EJ-David <daviderhabor.de@gmail.com>
2022-07-21 00:50:55 +0100

    search bar and city's names Auto-complete completed(pun intended)

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ae7cfd5d13mshefc78a3bddacc50p16ef84jsn9c141ab73cd6',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+
+
+
