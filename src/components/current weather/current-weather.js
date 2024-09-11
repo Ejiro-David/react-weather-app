9b685d0f9fdfc0d3c6876e08abc7a8f51dc39855
ejiro-david <daviderhabor.de@gmail.com>
2024-09-11 10:03:56 +0100

    just checking

--- src/components/current weather/current-weather.js
+++ src/components/current weather/current-weather.js
@@ -1,38 +1,47 @@
 import "./current-weather.css";
 
-const CurrentWeather = ({weatherDetails}) => {
-
+const CurrentWeather = ({ weatherDetails }) => {
   return (
     <div className="weather">
       <div className="top">
         <div>
           <p className="city">{weatherDetails.city}</p>
-          <p className="weather-description">{weatherDetails.weather[0].description}</p>
+          <p className="weather-description">
+            {weatherDetails.weather[0].description}
+          </p>
         </div>
-        <img alt="weather" className="weather-icon" src={`icons/${weatherDetails.weather[0].icon}.png`}></img>
+        <img
+          alt="weather"
+          className="weather-icon"
+          src={`icons/${weatherDetails.weather[0].icon}.png`}
+        ></img>
       </div>
       <div className="bottom">
         <p className="temp">{`${Math.round(weatherDetails.main.temp)}°C`}</p>
         <div className="details">
-            <div className="row">
-                <span className="label details-heading" id="">Details</span>
-            </div>
-            <div className="row">
-                <span className="label">Humidity</span>
-                <span className="value">{`${weatherDetails.main.humidity}%`}</span>
-            </div>
-            <div className="row">
-                <span className="label">Feels Like</span>
-                <span className="value">{`${Math.round(weatherDetails.main.feels_like)}°C`}</span>
-            </div>
-            <div className="row">
-                <span className="label">Wind</span>
-                <span className="value">{weatherDetails.wind.speed}m/s</span>
-            </div>
-            <div className="row">
-                <span className="label">Pressure</span>
-                <span className="value">{weatherDetails.main.pressure}hPa</span>
-            </div>
+          <div className="row">
+            <span className="label details-heading" id="">
+              Details
+            </span>
+          </div>
+          <div className="row">
+            <span className="label">Humidity</span>
+            <span className="value">{`${weatherDetails.main.humidity}%`}</span>
+          </div>
+          <div className="row">
+            <span className="label">Feels Like</span>
+            <span className="value">{`${Math.round(
+              weatherDetails.main.feels_like
+            )}°C`}</span>
+          </div>
+          <div className="row">
+            <span className="label">Wind</span>
+            <span className="value">{weatherDetails.wind.speed}m/s</span>
+          </div>
+          <div className="row">
+            <span className="label">Pressure</span>
+            <span className="value">{weatherDetails.main.pressure}hPa</span>
+          </div>
         </div>
       </div>
     </div>
